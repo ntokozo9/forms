b809c921160fb2746a88a8100dba5920fd8680f5
ntokozo9 <ntokozoblessingradebe98@gmail.com>
2023-04-12 08:55:39 +0200

    create conversions

---
+++ conversions.js
@@ -0,0 +1,24 @@
+//convert 100 pounds to kg
+const convertPoundsToKilograms = (pounds) => {
+  return pounds * 0.454;
+};
+
+console.log(convertPoundsToKilograms(100));
+
+//convert 100 inches to cm
+const convertInchesToCentimeters = (inches) => {
+  return inches * 2.54;
+};
+console.log(convertInchesToCentimeters(100));
+
+//convert 100 farhenheit to celcius
+const convertFarhenheitTocelcius = (farhenheit) => {
+  return (farhenheit-32) *5/9;
+};
+console.log(convertFarhenheitTocelcius(100));
+
+//convert 100 feet to meters
+const convertFeetToMeters = (feet) => {
+  return feet * 0.305;
+};
+console.log(convertFeetToMeters(100));
---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  let email = document.getElementById("Email1").value;
+  let name = document.getElementById("Name1").value;
+  let surname = document.getElementById("surname1").value;
+
+  console.log(email);
+  console.log(name);
+  console.log(surname);
+}
